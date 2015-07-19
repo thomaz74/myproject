ae9a5deb38f68bcfcb700d6e4a104b4d8c47cd2d
thomaz74 <tomshan234@gmail.com>
2015-07-20 00:14:55 +0100

    test

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// hello this is my script
+
 public class playercontroller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	//hello world
 	// Update is called once per frame
 	void Update () {
 	
